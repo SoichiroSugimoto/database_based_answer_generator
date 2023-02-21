8d5ef3353f14f121d1d3b65c19dfb757c1ac4e5e
SoichiroSugimoto <fcsoichi1998@gmail.com>
2023-02-22 01:48:22 +0900

    tmp

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,5 +1,9 @@
+import os
+import json
+import boto3
 import MeCab
 text = "静かなる鏡のごとき湖が眠れるアリチアの森"
 
-tagger = MeCab.Tagger()
-print(tagger.parse(text))
+def lambda_handler(event, context):
+	tagger = MeCab.Tagger()
+	print(tagger.parse(text))
