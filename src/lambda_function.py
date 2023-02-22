499b8c141f2b958019eb0faab8a6c3a3f9350774
SoichiroSugimoto <fcsoichi1998@gmail.com>
2023-02-23 02:25:57 +0900

    generate lexer array

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,9 +1,15 @@
 import os
 import json
+import csv
 # import boto3
 import MeCab
 
 def lambda_handler(event, context):
   text = "静かなる鏡のごとき湖が眠れるアリチアの森"
   tagger = MeCab.Tagger()
-  print(tagger.parse(text))
+  lexer_result = tagger.parse(text)
+  word_array = lexer_result.splitlines()
+  lexer_array = []
+  for a in word_array:
+    lexer_array.append(a.split('\t'))
+  print(lexer_array[2][4])
