fff0182b2f5d84783d98f0ccd9e51f0d12952e00
SoichiroSugimoto <fcsoichi1998@gmail.com>
2023-02-23 01:43:23 +0900

    to execute main.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,9 +1,9 @@
 import os
 import json
-import boto3
+# import boto3
 import MeCab
-text = "静かなる鏡のごとき湖が眠れるアリチアの森"
 
 def lambda_handler(event, context):
-	tagger = MeCab.Tagger()
-	print(tagger.parse(text))
+  text = "静かなる鏡のごとき湖が眠れるアリチアの森"
+  tagger = MeCab.Tagger()
+  print(tagger.parse(text))
