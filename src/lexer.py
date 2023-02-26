58e5bad0f4efdacd8f877b1b70ac99754a421821
SoichiroSugimoto <fcsoichi1998@gmail.com>
2023-02-26 10:52:26 +0900

    get noun array

--- src/lexer.py
+++ src/lexer.py
@@ -1,5 +1,7 @@
 import MeCab
 
+NOUN = "名詞-普通名詞-一般"
+
 def lexer(prompt):
   tagger = MeCab.Tagger()
   lexer_result = tagger.parse(text)
@@ -8,3 +10,11 @@ def lexer(prompt):
   for a in word_array:
     lexer_array.append(a.split('\t'))
   return (lexer_array)
+
+def get_noun(prompt):
+	lexer_array = lexer(prompt)
+	noun_array = []
+	for la in lexer_array:
+		if (la[0] != "EOS" and la[4] == "名詞-普通名詞-一般"):
+			noun_array.append(la[0])
+	print(noun_array)
