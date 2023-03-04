02134e1b92741ac6d5effbe81e6c13d7e993e156
SoichiroSugimoto <fcsoichi1998@gmail.com>
2023-03-05 02:53:29 +0900

    insert_resource_record

--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,23 @@
 import json
-from resource_model import Resource
+import lexer
 
+from resource_model import Resource
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, NumberAttribute, MapAttribute
 
 resources = dynamodb.Table('Resource')
 
-def generate_search_key(text):
-
-def insert_resource_record(prompt, completion, username):
+def insert_resource_record(source_text, means, username):
   dt = datetime.datetime.now()
   ts = datetime.datetime.timestamp(dt)
-  res = trainings.put_item(
+  search_key = lexer.generate_search_key(source_text)
+  res = resources.put_item(
     Item = {
       "sentence_id": ts,
-      "search_key": prompt,
-      "completion": completion,
-      "user": username
+      "search_key": search_key,
+      "sentence_text": source_text,
+      "created_with": means,
+      "created_byr": username
     }
   )
   return (res)
-  sentence_id = UnicodeAttribute(hash_key=True)
-  search_key = UnicodeAttribute(range_key=True)
-  sentence_text = UnicodeAttribute()
-  created_with = UnicodeAttribute()
-  created_by = UnicodeAttribute()
