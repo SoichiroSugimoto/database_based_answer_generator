ff913948c4e9195b7405f60506b90e9ad4c7e0c4
SoichiroSugimoto <fcsoichi1998@gmail.com>
2023-03-05 02:52:07 +0900

    change lexer argument name

--- src/lexer.py
+++ src/lexer.py
@@ -2,7 +2,7 @@ import MeCab
 
 NOUN = "名詞-普通名詞-一般"
 
-def lexer(prompt):
+def lexer(text):
   tagger = MeCab.Tagger()
   lexer_result = tagger.parse(text)
   word_array = lexer_result.splitlines()
@@ -11,17 +11,17 @@ def lexer(prompt):
     lexer_array.append(a.split('\t'))
   return (lexer_array)
 
-def get_noun(prompt):
-	lexer_array = lexer(prompt)
+def get_noun(text):
+	lexer_array = lexer(text)
 	noun_array = []
 	for la in lexer_array:
 		if (la[0] != "EOS" and la[4] == NOUN):
 			noun_array.append(la[0])
 	print(noun_array)
 
-def generate_search_key(prompt):
+def generate_search_key(text):
   search_key = ''
-  noun_array = get_noun(prompt):
+  noun_array = get_noun(text):
   for word in noun_array:
     if (search_key != ''):
       search_key += '_'
